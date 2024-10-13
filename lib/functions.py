b717b1dcd7ad94ad9e5d7a27af6a239596de5958
Reagan-Ongaya <reagan.ongaya@student.moringaschool.com>
2024-10-13 14:19:58 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print('Hello developer')
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f'Hello, {name}')
+greet('Reagan')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'hello , {name}')
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    print(
+        num1 + num2
+    )
+
+add( 3, 5)
 
 def halve(number):
-    pass
+    print(
+        int(number/2)
+    )
+halve(8)
